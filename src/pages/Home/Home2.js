970748d7a1bfc08a1dfa6db5303b0551517c20e6
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-27 00:32:44 -0400

    MAJ

---
+++ src/pages/Home/Home2.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Home2() {
+	useEffect(() => {
+		// Set cookie
+		fetch('https://serveur.dwighthaul.net/set-cookie', {
+			method: 'GET',
+			credentials: 'include',
+		})
+			.then(() => {
+				// Get cookie
+				return fetch('https://serveur.dwighthaul.net/get-cookie', {
+					method: 'GET',
+					credentials: 'include',
+				});
+			})
+			.then(response => response.json())
+			.then(data => console.log('Cookie received:', data))
+			.catch(error => console.error('Error:', error));
+	}, []);
+
+	return (
+		<div>
+			Check console for cookie data.
+		</div>
+	);
+}
