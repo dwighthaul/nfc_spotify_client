5d3b9db2c1dab5f3b856e52c4c2a86016903b9e1
paulohubert.ph@hotmail.fr <paulohubert.ph@hotmail.fr>
2024-05-27 00:43:40 -0400

    MAJ

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import './AppCss.css';
 import Navbar from './components/structure/Navbar/Navbar.js';
-import {Home, Playlists, SignIn, Page404, Dashboard, SignOut, Settings } from './pages'
+import { Home, Playlists, SignIn, Page404, Dashboard, SignOut, Settings } from './pages'
 import { UserProvider } from './context/userContext.jsx';
 import { UserSettingsProvider } from './context/userSettingsContext.jsx';
 
@@ -13,25 +13,26 @@ export default class App extends React.Component {
     return (
       <BrowserRouter>
         <UserProvider>
-        <UserSettingsProvider>
-        <Navbar />
-        <div className='app'>
-          <div className='app-container'>
-            <Routes>
-              <Route path='/' exact element={<Home />} />
-              <Route path='/home' exact element={<Home />} />
-              <Route path='/playlist' exact element={<Playlists />} />
-              <Route path='/sign-in' element={<SignIn />} />
-              <Route path='/settings' element={<Settings />} />
-              <Route path='/404' element={<Page404 />} />
-              <Route path='/logout' element={<SignOut />} />
-              <Route path='/dashboard' element={<Dashboard />} />
-              <Route path='*' element={<Page404 />} />
-            </Routes>
-          </div>
-        </div>
-        </UserSettingsProvider>
-        </UserProvider> 
+          <UserSettingsProvider>
+            <Navbar />
+            <div className='app'>
+              <div className='app-container'>
+                <Routes>
+                  <Route path='/' exact element={<Home />} />
+                  <Route path='/home' exact element={<Home />} />
+                  <Route path='/home2' exact element={<Home2 />} />
+                  <Route path='/playlist' exact element={<Playlists />} />
+                  <Route path='/sign-in' element={<SignIn />} />
+                  <Route path='/settings' element={<Settings />} />
+                  <Route path='/404' element={<Page404 />} />
+                  <Route path='/logout' element={<SignOut />} />
+                  <Route path='/dashboard' element={<Dashboard />} />
+                  <Route path='*' element={<Page404 />} />
+                </Routes>
+              </div>
+            </div>
+          </UserSettingsProvider>
+        </UserProvider>
       </BrowserRouter>
     );
   }
